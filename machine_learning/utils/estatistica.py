0f0ec54c05242f6b586f57eb69be334ad3d746dc
diego <diego.felipe095@gmail.com>
2024-07-17 18:18:10 -0300

    criação das funções da tabela de contigência e teste chi2 no módulo de estatística

--- machine_learning/utils/estatistica.py
+++ machine_learning/utils/estatistica.py
@@ -1,1 +1,29 @@
-import pandas as pd 
+import pandas as pd
+from typing import Series
+from typing import DataFrame
+from scipy.stats import chi2_contingency
+
+
+def tabela_de_contingencia(variavel1: Series[float], variavel2: Series[float]) -> DataFrame[float]:
+    """Função que gera a tabela de contingência - frequências com que cada combinação
+      de categorias aparece em um conjunto de dados.
+
+    Args:
+        variavel1 (Series[float]): _description_
+        variavel2 (Series[float]): _description_
+
+    Returns:
+        DataFrame[float]: Tabela de contingência
+    """
+    return pd.crosstab(variavel1, variavel2)
+
+def chi2_tabela_de_contingecia(tabela_contingencia:DataFrame[float]) -> object: 
+    """Analisa a significância estatística da associação entre as variáveis (teste qui²)
+
+    Args:
+        tabela_contingencia (DataFrame[float]): tabela contendo as fequências observadas
+
+    Returns:
+        object: contendo a estatística de teste, p-value e as frequências esperadas 
+    """
+    return chi2_contingency(tabela_contingencia)
